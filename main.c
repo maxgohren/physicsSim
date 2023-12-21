885ef1a845b4ed74c83b9833daae665448802ab7
maxgohren <maxgohren@gmail.com>
2023-12-21 13:34:11 -0500

    working on physics engine

--- main.c
+++ main.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <termios.h> 
+#include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <termios.h>
 
 #define mapSizeX 25
 #define mapSizeY 50
-
+#define FRAMERATE 3
 
 struct termios oldt,newt;
 
-// struct Cannon {
-// 	int angle;
-//	int startVelocity; //split into x and y?
-//	int cannonBallMass;
-//}cannon;
-
 struct Ball {	
-	int x;
-	int y;
-	int prevX;
-	int prevY;
-	int initialVelocity;
-	int angle; 
-}ball = {0, 0, 0,0, 10, 45};
+	double x;
+	double y;
+	double prevX;
+	double prevY;
+	double vx;
+	double vy;
+	double initialVelocity;
+	double angle; 
+}ball = {0, 0, 0, 0, 0, 0, 12/FRAMERATE, 45};
 
 char userInput = -1; 
 char menuInput = -1;
 char ballInput = 0;
 char map[mapSizeX][mapSizeY]; 
 int simulationTime = 0;
-
+double gravity = -5 / FRAMERATE;
+int count = 0;
 
 void setNonCanonicalMode();
 void setCanonicalMode();
@@ -41,13 +38,21 @@ void updatePhysicsSim();
 void fillMap();
 void userMoveBall();
 
-void setMapXY(int x, int y, char c) {map[y][x] = c;}
-void setBallPos(int x,int y) {map[y][x] = '@';}
+void setMapXY(double x, double y, char c)
+{
+	if(ball.x < mapSizeX && ball.x >= 0 && ball.y < mapSizeY && ball.x >= 0)
+		map[(int)y][(int)x] = c;
+}
+
+void setBallPos(double x,double y) 
+{
+	if(ball.x < mapSizeX && ball.x >= 0 && ball.y < mapSizeY && ball.x >= 0)
+		map[(int)y][(int)x] = '@';
+}
 
 int main()
 {
-	//init
-	fillMap();
+	init(); 
 
 	//menu screen
 	clearScreen();
@@ -67,16 +72,13 @@ int main()
 	{
 		clearScreen();
 		drawPhysicsSim();
-
 		read(STDIN_FILENO,&userInput,1); 
 
-		//simulation time is currently tied to framerate
-		simulationTime++;
-		printf("The current simulation time is: %d\n", simulationTime);
 		//wait for user to view simulation and update for next frame
-		sleep(1);
-		updatePhysicsSim(); 
-		
+		sleep(1/FRAMERATE);
+
+		updatePhysicsSim();
+		simulationTime++;
 	}
 	clearScreen();
 	setCanonicalMode();
@@ -90,8 +92,9 @@ void clearScreen()
 	printf("\e[2J\e[H"); 
 }
 
-void fillMap()
+void init()
 {
+	//fill map with dots
 	for(int i = 0; i < mapSizeX; i++)
 	{
 		for(int j = 0; j < mapSizeY; j++)
@@ -99,6 +102,10 @@ void fillMap()
 			map[i][j] = '.';
 		}	
 	}
+	
+	//calculate initial velocity components	
+	ball.vx = ball.initialVelocity * cos( (double) ball.angle) / FRAMERATE;
+	ball.vy = ball.initialVelocity * sin( (double) ball.angle) / FRAMERATE;
 //draw screen with green grass and blue air
 //draw cannon on bottom left of screen
 //show angle in degrees 
@@ -113,16 +120,17 @@ void drawMenu()
 {
 	printf("Welcome to the physics simulation!\n"); 
 	printf("Please choose an option:\n\n");
-	printf("s -> start the simulation\n");	
-	printf("q -> quit\n");	
+	printf("	s -> start the simulation\n");	
+	printf("	q -> quit\n");	
 }
 
 
 
 void drawPhysicsSim()
-{
+{	
 	setBallPos(ball.x, ball.y);
 
+	//printing rows so that 0 -> mapSizeX row is printed (similar to x increasing upwards)
 	for(int i = mapSizeX - 1; i >= 0; i--)
 	{
 		for(int j = 0; j < mapSizeY; j++)
@@ -131,45 +139,30 @@ void drawPhysicsSim()
 		}
 		printf("\n");
 	}
+	
+	printf("Simulation Time: %d seconds \n", simulationTime/FRAMERATE);
+	printf("Cannon Angle: %g degrees\n", ball.angle);
+	printf("Cannon Velocity: %g m/s\n", ball.initialVelocity);
+	printf("Ball Position (x,y) = (%g m, %g m)\n", ball.y, ball.x);
+	printf("Ball Velocity (x,y) = (%g m, %g m)\n", ball.vy, ball.vx);
+	printf("Gravity: %g m/s\n", gravity);
+
+	printf("\n");
 }
 
 void updatePhysicsSim()
 {
-	//drawing ballZ
-	
+	//update velocities due to accelerations
+	//ball.vx = air resistance
+	ball.vy += gravity / FRAMERATE;
+
 	//store previous location to clear
 	ball.prevX = ball.x;
 	ball.prevY = ball.y;	
-	/*	
-	//calculate next ball position based on velocity and angle
-	int dx, dy, vx, vy, t1, t2;
-	
-	t1 = simulationTime;
-	t2 = simulationTime + 1;
-	//vx = v * sin(cannon.angle); // needs to be converted to degrees for percentage
-	//vy = v * cos(cannon.angle);
+	//update ball position
+	ball.x += ball.vx / FRAMERATE; 
+	ball.y += ball.vy / FRAMERATE;
 
-	vx = ball.initialVelocity / 2;
-	vy = ball.initialVelocity / 2;
-	
-	dx = vx * (t2 - t1);
-	dy = vy * (t2 - t1);
-
-	//store new position in map		
-	*/ 
-	//move ball diagonally across the screen
-	
-	static int diag = 0;	
-	if(diag ==0)
-	{
-		ball.x++;
-		diag = 1;
-	}
-	else
-	{
-		ball.y++;
-		diag = 0;
-	}
 	//clear old ball position
 	if(ball.x != ball.prevX || ball.y != ball.prevY) setMapXY(ball.prevX, ball.prevY, '.');
 	
